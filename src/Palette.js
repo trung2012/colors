7ee0bc8b84f615a4f84e507584bee9b86c5f45e4
Trung Pham <trungpham2012@gmail.com>
2019-07-01 16:08:38 -0500

    Add Copy Overlay Animation

---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
+
+export default class Palette extends Component {
+	render() {
+		const colorBoxes = this.props.colors.map(color => {
+			return <ColorBox background={color.color} name={color.name}/>
+		});
+
+		return (
+			<div className='Palette'>
+				<div className='Palette-colors'>
+					{colorBoxes}
+				</div>
+			</div>
+		);
+	}
+}
