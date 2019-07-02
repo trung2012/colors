85f4ab6154cdbc5325703deb81b69f62bca8330d
Trung Pham <trungpham2012@gmail.com>
2019-07-02 15:49:28 -0500

    Add Snackbar and styled Select

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
 	state = {
-		format: 'hex'
+		format: 'hex',
+		open: false
 	}
 
-	handleChange = (event) => {
-	this.setState({format:event.target.value});
+	handleFormatChange = (event) => {
+	this.setState({format :event.target.value, open:true});
 	this.props.handleChange(event.target.value);
+	}
+
+	closeSnackbar = () => {
+		this.setState({open:false});
 	}
 
 	render() {
@@ -36,12 +44,32 @@ class Navbar extends Component {
 					</div>	
 				</div>
 				<div className='select-container'>
-					<Select value={format} onChange={this.handleChange}>
+					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value='hex'>HEX-#ffffff</MenuItem>
 						<MenuItem value='rgb'>rgb(255,255,255)</MenuItem>
 						<MenuItem value='rgba'>rgba(255,255,255,1.0)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar 
+				anchorOrigin={{vertical:'bottom', horizontal:'left'}}
+				open={this.state.open}
+				autoHideDuration={3000}
+				message={<span id='message-id'>Format Changed!</span>}
+				ContentPrpos={{
+					'aria-describedby': 'message-id'
+				}}
+				onClose={this.closeSnackbar}
+				action ={[
+					<IconButton 
+					onClick={this.closeSnackbar} 
+					color='inherit' 
+					key='close'
+					aria-label='close' 
+					>
+						<CloseIcon/>
+					</IconButton>
+				]}
+				/>
 			</header>
 		);
 	}
